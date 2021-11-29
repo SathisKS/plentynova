8119a0185ba6a9c77460058ce48adba7ec3a8364
Sathis <49578962+SathisKS@users.noreply.github.com>
2021-11-29 16:34:55 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -828,9 +828,9 @@ class PaymentService
                      $paymentData['due_date'] = !empty($responseData['due_date']) ? $responseData['due_date'] : $bankDetails->due_date;
                      $paymentData['payment_id'] = $key;
                  } 
-               $transactionComments .= PHP_EOL . sprintf($this->paymentHelper->getTranslatedText('transaction_confirmation'), date('d.m.Y'), date('H:i:s'));
+               $transactionComments .= PHP_EOL . sprintf($this->paymentHelper->getTranslatedText('transaction_confirmation', $paymentRequestData['lang']), date('d.m.Y'), date('H:i:s'));
            } else {
-            $transactionComments .= PHP_EOL . sprintf($this->paymentHelper->getTranslatedText('transaction_cancel'), date('d.m.Y'), date('H:i:s'));
+            $transactionComments .= PHP_EOL . sprintf($this->paymentHelper->getTranslatedText('transaction_cancel', $paymentRequestData['lang']), date('d.m.Y'), date('H:i:s'));
         }
              if (($responseData['tid_status'] == '100' && $key == '27') || $responseData['tid_status'] != '100') {
              $paymentData['paid_amount'] = 0;
