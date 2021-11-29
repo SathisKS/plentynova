1345247f9eecba0fc3a6b1e2582f13f271ba4257
Sathis <49578962+SathisKS@users.noreply.github.com>
2021-11-29 12:57:59 +0530

    Update NovalnetSepaPaymentMethod.php

--- src/Methods/NovalnetSepaPaymentMethod.php
+++ src/Methods/NovalnetSepaPaymentMethod.php
@@ -76,7 +76,7 @@ class NovalnetSepaPaymentMethod extends PaymentMethodService
      */
     public function isActive():bool
     {
-       if ($this->configRepository->get('Novalnet.novalnet_sepa_payment_active') == true) {
+       if ($this->configRepository->get('Novalnet.novalnet_sepa_payment_active') == 'true') {
         
         $active_payment_allowed_country = true;
         if ($allowed_country = $this->configRepository->get('Novalnet.novalnet_sepa_allowed_country')) {
